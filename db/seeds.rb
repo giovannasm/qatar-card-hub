2dd270270ea847c1c1f4afc610d606ea287cc762
Giovanna Montemurro <giovannasm@gmail.com>
2022-08-23 15:09:23 -0300

    seeds prontos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# 30.times do
-#   Offer.create!(number: rand(1..100),
-#                 player_name: 'Neymar',
-#                 team: "Brasil",
-#                 special: false,
-#                 description: 'Quero qualquer outro jogador',
-#                 user_id: 2,
-#                 photo: )
-# end
+require 'uri'
+
+url = 'https://asset.cloudinary.com/giovannasm/e30181383c52cb97d6dda5b27fec2fd5'
+
+30.times do
+  offer = Offer.new(number: rand(1..100),
+                player_name: 'Neymar',
+                team: "Brasil",
+                special: false,
+                description: 'Quero qualquer outro jogador',
+                user_id: 2)
+
+  file = URI.open(url)
+  offer.photo.attach(io: file, filename: 'Neymar', content_type: 'image/jpg')
+  offer.save
+end
