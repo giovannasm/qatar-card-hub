9772370ab30e83f10f9057d741b78b4d07a1ecb1
Giovanna Montemurro <giovannasm@gmail.com>
2022-08-24 10:55:25 -0300

    offers liberadas sem login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-  
+  skip_before_action :authenticate_user!, only: %i[home offers]
+
   def home
   end
 end
