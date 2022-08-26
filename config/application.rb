722e12b5441dc7f47d2003a606521202ed55b396
Giovanna Montemurro <giovannasm@gmail.com>
2022-08-26 15:20:06 -0300

    mudança na imagem

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module QatarCardHub
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.compile = true 
   end
 end
