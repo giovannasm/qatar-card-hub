9eb7c9ed44d99a332af9200d0387167391fffe30
Giovanna Montemurro <giovannasm@gmail.com>
2022-08-27 11:39:05 -0300

    navbar com js

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
+    console.log("hello from nav controller")
   }
 
   updateNavbar() {
